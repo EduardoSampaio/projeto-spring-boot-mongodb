d4294e3b4580bf1501b21f8555ac6ff0700befd0
Eduardo <esampaio@tecgraf.puc-rio.br>
2018-12-11 17:52:18 -0200

    Projeção dos dados do autor com DTO

--- src/main/java/com/spring/boot/mongodb/config/Instantiation.java
+++ src/main/java/com/spring/boot/mongodb/config/Instantiation.java
@@ -2,11 +2,17 @@ package com.spring.boot.mongodb.config;
 
 import static java.util.Arrays.asList;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.spring.boot.mongodb.domain.Post;
 import com.spring.boot.mongodb.domain.User;
+import com.spring.boot.mongodb.dtos.AuthorDTO;
+import com.spring.boot.mongodb.repository.PostRepository;
 import com.spring.boot.mongodb.repository.UserRepository;
 
 @Configuration
@@ -15,15 +21,37 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userReposiroty;
 	
+	@Autowired
+	private PostRepository postReposiroty;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userReposiroty.saveAll(asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+//		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+//		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+//		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+//		post1.getComments().addAll(Arrays.asList(c1, c2));
+//		post2.getComments().addAll(Arrays.asList(c3));
+//		
+		postReposiroty.saveAll(asList(post1, post2));
+//		
+//		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(maria);
 		
 	}
 
--- src/main/java/com/spring/boot/mongodb/repository/PostRepository.java
+++ src/main/java/com/spring/boot/mongodb/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.spring.boot.mongodb.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.spring.boot.mongodb.domain.User;
+import com.spring.boot.mongodb.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-	
+public interface PostRepository extends MongoRepository<Post, String> {
+
 }
